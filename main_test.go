e69c58d95a102797caf617a8f3739ba12b956dbc
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-10-08 01:09:02 +0530

    wrote tests

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	rootEndpointResponse string = "<html><body><a href=\"https://renovatio-design.vercel.app/\">Renovatio </a></body></html>"
+)
+
+func TestRoot(t *testing.T) {
+	t.Run("testing root endpoint", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		responseRecorder := httptest.NewRecorder()
+
+		Home(responseRecorder, request)
+
+		response := responseRecorder.Body.String()
+
+		if response != rootEndpointResponse {
+			t.Errorf("got: %s\nwant: %s", response, rootEndpointResponse)
+		}
+	})
+}
