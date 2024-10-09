c70abf4678ce2678a236eecdb5c05233972fa571
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-10-09 16:54:02 +0530

    localhost

--- main.go
+++ main.go
@@ -4,33 +4,17 @@ import (
 	"errors"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/mshra/renovatioBackend/handlers"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	var PORT string
-
-	PORT, isPORT := os.LookupEnv("PORT")
-
-	if !isPORT {
-		PORT = "localhost:8000"
-	}
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", handlers.Home)
 
 	log.Print("-- Server running on PORT:8000")
-	err = http.ListenAndServe(PORT, mux)
+	err := http.ListenAndServe("localhost:8000", mux)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		log.Fatal("Server Closed!")
